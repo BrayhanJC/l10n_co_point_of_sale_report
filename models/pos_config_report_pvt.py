cd179ecf7c57605e143e2b3099487b34e68ac419
BrayhanJC <brayhanjaramillo@hotmail.com>
2019-12-20 23:07:11 -0500

    Cambios en la captura de la fecha final

--- models/pos_config_report_pvt.py
+++ models/pos_config_report_pvt.py
@@ -90,6 +90,9 @@ class PosConfigReportPDV(models.TransientModel):
 		date_last_thirty = today + timedelta(days=-30)
 		date_last_thirty = self.return_date_current(date_last_thirty)
 
+		if self.date_end:
+			today = self.date_end
+
 		sql_delete = "DELETE FROM pos_report_pvt"
 		self.env.cr.execute(sql_delete)
 
